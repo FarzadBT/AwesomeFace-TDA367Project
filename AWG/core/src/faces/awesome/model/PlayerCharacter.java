f15f62971d96109e4f04d16e5b5c5044713300f9
Amloudhi <philip.nil@gmail.com>
2018-04-27 15:41:56 +0200

    Renamed player to playercharacter

---
+++ AWG/core/src/faces/awesome/model/PlayerCharacter.java
@@ -0,0 +1,41 @@
+package faces.awesome.model;
+
+import faces.awesome.AwesomeGame;
+
+import java.lang.*;
+
+public class PlayerCharacter extends Character {
+
+	private Facing facing;
+
+	public PlayerCharacter(Position pos){
+		super(pos);
+	}
+
+
+
+	@Override
+	protected void attack(Character other) {
+
+	}
+
+
+	public void move(float dx, float dy) {
+
+		//Moves the player
+		setPos(pos.movePos(dx, dy));
+
+	}
+/*
+	public boolean isWithinCamView() {
+		float x = getPos().getX();
+		float y = getPos().getY();
+
+		if (x >  && ) {
+
+		}
+	}
+*/
+
+
+}
