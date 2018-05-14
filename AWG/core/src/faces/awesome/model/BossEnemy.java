4ead4c08ef84e3c5a3623f58426b2489572284c0
Amloudhi <philip.nil@gmail.com>
2018-05-14 19:57:17 +0200

    Added a class that represents a boss enemy. Simply a stronger enemy type

---
+++ AWG/core/src/faces/awesome/model/BossEnemy.java
@@ -0,0 +1,23 @@
+package faces.awesome.model;
+
+import faces.awesome.AwesomeGame;
+
+/* This class represents a boss enemy. Simply a stronger form of enemy. If this dies, the player "wins" the game.*/
+
+public class BossEnemy extends Enemy {
+
+    public BossEnemy(Position pos, AwesomeGame game) {
+        super(pos, game);
+        baseDamage = 5;
+        health = maxHealth*2;
+    }
+
+    public boolean checkDeath(){
+        if(this.getHealth() == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+}
