df09ba4015d574bcc8b6b453a4d52605e6eda1f2
Phones <farmerfaz@gmail.com>
2018-05-22 09:54:17 +0200

    Started work the logic for BombObject

--- AWG/core/src/faces/awesome/model/objects/object/BombObject.java
+++ AWG/core/src/faces/awesome/model/objects/object/BombObject.java
@@ -1,8 +1,11 @@
 package faces.awesome.model.objects.object;
 
+import faces.awesome.model.Enemy;
 import faces.awesome.model.GameObject;
+import faces.awesome.model.MapSegment;
 import faces.awesome.model.Position;
 
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,11 +21,17 @@ public class BombObject extends GameObject {
     }
 
     private void countDown() {
+        int x = pos.getX();
+        int y = pos.getY();
+
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-
+                List<Enemy> targets = MapSegment.getPlayerTargets(x-1, y+1, x+1, y-1);
+                for (Enemy enemy: targets) {
+                    enemy.decreaseHealth(10);
+                }
             }
         }, 1500);
     }
