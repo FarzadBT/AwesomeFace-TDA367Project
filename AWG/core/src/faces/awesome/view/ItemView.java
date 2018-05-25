7da20ec06de429418d4330271db2b4bc740965f9
Therese Sturesson <thestu@student.chalmers.se>
2018-05-25 15:54:49 +0200

    added some öast details

--- AWG/core/src/faces/awesome/view/ItemView.java
+++ AWG/core/src/faces/awesome/view/ItemView.java
@@ -10,7 +10,7 @@ import faces.awesome.model.GameObject;
  * @author Linus Wallman
  *
  * TODO vad gör klassen?
- * TODO används inte? spara?
+ * TODO används inte?
  */
 
 public class ItemView extends GameObjectView {
